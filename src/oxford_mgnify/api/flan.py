fa6389d4a5f1cd94b1cdd53eafd55de003edc6a6
aabdullah27182845 <abdullahnavid5@gmail.com>
2025-04-02 15:41:59 +0100

    test changes

---
+++ src/oxford_mgnify/api/flan.py
@@ -0,0 +1,11 @@
+
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-small")
+model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-small")
+
+input_text = "What does SQL mean?"
+input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+
+outputs = model.generate(input_ids)
+print(tokenizer.decode(outputs[0]))
